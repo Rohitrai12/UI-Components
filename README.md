1338aaaa9c0352e62e986f16cf0bcaa985f9f88d
David Mraz <david@atheros.ai>
2022-11-03 12:06:23 +0100

    feat: initial repo

---
+++ README.md
@@ -0,0 +1,2 @@
+This is repository with React examples of simple UI components. The repository is based on Next.js and React.js. 
+
---
+++ pages/_app.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import globalStyle from '../components/global.scss';
+
+interface IAppProps {
+  Component: React.FunctionComponent;
+  pageProps: Record<string, unknown>;
+}
+
+const App: React.FunctionComponent<IAppProps> = ({
+  Component,
+  pageProps,
+}: IAppProps) => {
+
+  return (
+    <div className={globalStyle.Global}>
+        <Component {...pageProps} />
+    </div>
+  );
+};
+
+export default App;
