151a848e772dc13e05721a0ba8f96d43d96ecac1
suhaotian <h4av.one@gmail.com>
2024-01-21 11:27:56 +1100

    fix next build

--- react-examples/next.config.js
+++ react-examples/next.config.js
@@ -3,16 +3,7 @@
  */
 const nextConfig = {
   basePath: '/react-examples/out',
-  output: 'export',
- 
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
- 
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
- 
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
+  output: 'standalone'
 }
  
 module.exports = nextConfig
