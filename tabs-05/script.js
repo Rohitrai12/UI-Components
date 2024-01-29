2a24c77b59a129abd9dc91eca074dff8519ccc95
David Mraz <david@atheros.ai>
2024-01-29 13:37:51 +0100

    feat: tab 05

--- tabs-05/script.js
+++ tabs-05/script.js
@@ -1,17 +1,16 @@
-
 const allLinks = document.querySelectorAll(".tabs a");
 const allTabs = document.querySelectorAll(".tab-content");
 
 allLinks.forEach((elem) => {
-  elem.addEventListener('click', function() {
+  elem.addEventListener("click", function () {
     const linkId = elem.id;
     const hrefLinkClick = elem.href;
 
     allLinks.forEach((link) => {
-      if (link.href == hrefLinkClick){
+      if (link.href == hrefLinkClick) {
         link.classList.add("active");
       } else {
-        link.classList.remove('active');
+        link.classList.remove("active");
       }
     });
 
@@ -19,12 +18,9 @@ allLinks.forEach((elem) => {
       if (tab.id.includes(linkId)) {
         tab.classList.add("tab-content--active");
         // generate content for tab
-        generateTabItems(
-          elem,
-          tab
-        );    
+        generateTabItems(elem, tab);
       } else {
-        tab.classList.remove('tab-content--active');
+        tab.classList.remove("tab-content--active");
       }
     });
   });
@@ -32,54 +28,52 @@ allLinks.forEach((elem) => {
 
 const tabRecords = [
   {
-    src: 'assets/slack.png',
-    name: 'Slack',
-    site: 'https://slack.com',
-    description: 'Slack is a messaging app for business.',
-    type: ['communication', 'productivity'],
+    src: "assets/slack.png",
+    name: "Slack",
+    site: "https://slack.com",
+    description: "Slack is a messaging app for business.",
+    type: ["communication", "productivity"],
   },
   {
-    src: 'assets/sendgrid.png',
-    name: 'SendGrid',
-    site: 'https://sendgrid.com',
-    description: 'Email API service for developers',
-    type: ['developer'],
+    src: "assets/sendgrid.png",
+    name: "SendGrid",
+    site: "https://sendgrid.com",
+    description: "Email API service for developers",
+    type: ["developer"],
   },
   {
-    src: 'assets/asana.png',
-    name: 'Asana',
-    site: 'https://asana.com',
-    description: 'Track, manage and control your projects across any team, right',
-    type: ['productivity'],
+    src: "assets/asana.png",
+    name: "Asana",
+    site: "https://asana.com",
+    description:
+      "Track, manage and control your projects across any team, right",
+    type: ["productivity"],
   },
   {
-    src: 'assets/yahoo.png',
-    name: 'Yahoo Finance',
-    site: 'https://finance.yahoo.com',
-    description: 'Get quotes of stocks, funds, ETFs, currencies',
-    type: ['external'],
+    src: "assets/yahoo.png",
+    name: "Yahoo Finance",
+    site: "https://finance.yahoo.com",
+    description: "Get quotes of stocks, funds, ETFs, currencies",
+    type: ["external"],
   },
 ];
 
 //? predefined filter functions
 const filter = {
-  ['all']: () => true,
-  ['developer']: (record) => record.type.includes('developer'),
-  ['productivity']: (record) => record.type.includes('productivity'),
-  ['external']: (record) => record.type.includes('external'),
-  ['communication']: (record) => record.type.includes('communication'),
-}
+  ["all"]: () => true,
+  ["developer"]: (record) => record.type.includes("developer"),
+  ["productivity"]: (record) => record.type.includes("productivity"),
+  ["external"]: (record) => record.type.includes("external"),
+  ["communication"]: (record) => record.type.includes("communication"),
+};
 
 const generateTabItems = (elem, tabContent) => {
   const filterName = elem.name;
 
   const filterFunction = filter[filterName];
 
-  const mappedRecords = tabRecords
-    .filter(filterFunction)
-    .map(
-      (record) => {
-      return DOMPurify.sanitize(
+  const mappedRecords = tabRecords.filter(filterFunction).map((record) => {
+    return DOMPurify.sanitize(
       `<div class="record">
         <div class="avatar__wrapper">
           <img
@@ -114,11 +108,12 @@ const generateTabItems = (elem, tabContent) => {
               </span>
           </label>
         </div>
-      </div>`);
+      </div>`
+    );
   });
 
-  tabContent.innerHTML = mappedRecords.join('');
-}
+  tabContent.innerHTML = mappedRecords.join("");
+};
 
 //? handle proper selection for initial load
 const currentHash = window.location.hash;
@@ -126,20 +121,16 @@ const currentHash = window.location.hash;
 let activeLink = document.querySelector(`.tabs a`);
 
 if (currentHash) {
-  const visibleHash = document.getElementById(
-    `${currentHash}`
-  );
+  const visibleHash = document.getElementById(`${currentHash}`);
 
   if (visibleHash) {
     activeLink = visibleHash;
   }
 }
 
-const activeTab = document.querySelector(
-  `#${activeLink.id}-content`
-);
+const activeTab = document.querySelector(`#${activeLink.id}-content`);
 
-activeLink.classList.toggle('active');
-activeTab.classList.toggle('tab-content--active');
+activeLink.classList.toggle("active");
+activeTab.classList.toggle("tab-content--active");
 
 generateTabItems(activeLink, activeTab);
